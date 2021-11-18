57444c7585d62e84442eeebab494d7174bb14c40
Jan <janmichaelalcoseba@gmail.com>
2021-11-18 22:35:17 +0800

    Splash screen and Icon

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+   appBar: AppBar(
+      title: const Text('Kisuki Delivery'),
+     centerTitle: true,
+     backgroundColor: Colors.cyanAccent,
+   ),
+    body: const Center(
+      child: Text("Hello ki'"),
+    ),
+ ),
+));
